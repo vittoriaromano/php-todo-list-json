7ea5af1def807306902eb23c9856120044ec8a9e
vittoriaromano <vittoria5005@gmail.com>
2023-04-12 18:54:10 +0400

    first commit

---
+++ readme.md
@@ -0,0 +1,5 @@
+# Descrizione
+Creare una web-app che permetta di leggere e scrivere una lista di Todo.
+Gestita la persistenza dei dati leggendoli da, e scrivendoli in un file JSON.
+# Stack
+Html, CSS, VueJS (importato tramite CDN), axios, PHP
---
+++ server.php
@@ -0,0 +1,27 @@
+
+[
+    {
+        "text": "HTML",
+        "done": true
+    },
+    {
+        "text": "CSS",
+        "done": true
+    },
+    {
+        "text": "Responsive design",
+        "done": true
+    },
+    {
+        "text": "Javascript",
+        "done": true
+    },
+    {
+        "text": "PHP",
+        "done": true
+    },
+    {
+        "text": "Laravel",
+        "done": false
+    }
+]
