0293ad38555afaae9af7d0d4689fc640660481f4
vittoriaromano <vittoria5005@gmail.com>
2023-04-12 22:27:21 +0400

    invio-nuove-task

--- main.js
+++ main.js
@@ -1,16 +1,34 @@
-const {createApp} = Vue
+const { createApp } = Vue
 
 createApp({
-    data(){
-        return{
-           list: [], 
+    data() {
+        return {
+            list: [],
+            newItem: "",
         }
     },
-    created(){
+    methods: {
+        addItem() {
+            const data = {
+                newItem: this.newItem
+            };
+            //postare il nuovo item nel server e poi incorporare in lista
+            axios.post('server.php', data, {
+                headers: { 'Content-Type': 'multipart/form-data' }
+            })
+                .then((response) => {
+                    this.newItem = "";
+                    this.list = response.data;
+                    console.log(this.list)
+                })
+        }
+    },
+    created() {
+        //chiamata al server e stampa la risposta in lista
         axios.get('server.php')
-          .then((response) => {
-            this.list = response.data;
-            console.log(this.list)
-          })
+            .then((response) => {
+                this.list = response.data;
+                console.log(response)
+            })
     }
 }).mount('#app')
--- server.php
+++ server.php
@@ -1,5 +1,25 @@
 <?php
+//connessione file json
 $list = file_get_contents(__DIR__.'/list.json');
+//se post e settato rinominarlo new_item
+$new_item = isset($_POST['newItem']) ? $_POST['newItem'] : "";
 
+//se il post non e' vuoto convertiro da json a php
+if($new_item !== ""){
+    $list = json_decode($list, true);
+    //dato valore
+    $list[] = [
+        "text" => $new_item,
+        "done" => false
+    ];
+    //codifica dato valore in una stringa in sintassi JSON
+    $list = json_encode($list);
+    //aggiungere il contenuto del file nel json
+    file_put_contents(__DIR__.'/list.json', $list );
+}
+//informare http del formato data
 header('Content-Type: application/json');
+
+//stanpare dato
 echo $list; 
+
